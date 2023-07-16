90a64bd94ab5c4660434cc703e4c5f1b2214c6d7
Sujay <sujayv2016@gmail.com>
2023-07-16 12:22:30 +0530

    Got 70% of the JS to work, but main thing isn't working yet

--- scriptGame.js
+++ scriptGame.js
@@ -1,54 +1,103 @@
-function swapTiles(cell1,cell2) {
-    var temp = document.getElementById(cell1).className;
-    document.getElementById(cell1).className = document.getElementById(cell2).className;
-    document.getElementById(cell2).className = temp;
-  }
-  
-  function shuffle() {
-  //Use nested loops to access each cell of the 3x3 grid
-  for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
-     for (var column=1;column<=3;column++) { //For each column in this row
+var currTile;
+var otherTile;
+
+//Function to cut the image into m * n parts
+function cutImageIntoParts(image, m, n) {
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var width = image.width / n;
+    var height = image.height / m;
+    var parts = [];
     
-      var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
-      var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
-       
-      swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
-    } 
-  } 
-  }
+    /*var imgs=document.createElement("img");
+    imgs.src="./pics/avgengers.jpg";
+    var place=document.getElementById("retryes");
+    place.appendChild(imgs);*/ //this code is for appending images onto html
+    
+
+    canvas.width = width;
+    canvas.height = height;
   
-  function clickTile(row,column) {
-    var cell = document.getElementById("cell"+row+column);
-    var tile = cell.className;
-    if (tile!="tile9") { 
-         //Checking if white tile on the right
-         if (column<3) {
-           if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
-             swapTiles("cell"+row+column,"cell"+row+(column+1));
-             return;
-           }
-         }
-         //Checking if white tile on the left
-         if (column>1) {
-           if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
-             swapTiles("cell"+row+column,"cell"+row+(column-1));
-             return;
-           }
-         }
-           //Checking if white tile is above
-         if (row>1) {
-           if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
-             swapTiles("cell"+row+column,"cell"+(row-1)+column);
-             return;
-           }
-         }
-         //Checking if white tile is below
-         if (row<3) {
-           if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
-             swapTiles("cell"+row+column,"cell"+(row+1)+column);
-             return;
-           }
-         } 
+    for (var i = 0; i < m; i++) {
+      for (var j = 0; j < n; j++) {
+        context.clearRect(0, 0, width, height);
+        context.drawImage(
+          image,
+          j * width,
+          i * height,
+          width,
+          height,
+          0,
+          0,
+          width,
+          height
+        );
+  
+        // Create a new image object for each part
+        var part = new Image();
+        part.src = canvas.toDataURL();
+        parts.push(part);
+      }
     }
-    
+  
+    return parts;
   }
+  
+  // Example usage
+  var inputImage = new Image();
+  inputImage.src = "./pics/avgengers.jpg";
+  
+  inputImage.onload = function () {
+    var m = parseInt(prompt('Enter the number of rows (m):'));
+    var n = parseInt(prompt('Enter the number of columns (n):'));
+    var sizeHeight=120*n;var sizeWidth=120*m;
+    var imageParts = cutImageIntoParts(inputImage, m, n);
+    var a=document.getElementById("retryes");
+    a.style.height='${sizeHeight}';
+    a.style.width='${sizeWidth}';
+    
+    // Access each image part using the array index
+    for (var i = 0; i < imageParts.length; i++) {
+      var imagePart = imageParts[i];
+      var retriy= document.getElementById("retryes");
+      // Do something with each image part, such as displaying it on the page
+      retriy.appendChild(imagePart).className="tile"+(i+1);
+      var tile=document.getElementsByClassName("tile"+(i+1));
+      
+    }
+  }
+  
+
+
+window.onload=function(){
+  let tile=document.getElementById("cell");
+  
+  tile.addEventListener("dragstart",dragStart());
+  tile.addEventListener("dragend",dragEnd());
+  tile.addEventListener("dragenter",dragEnter());
+  tile.addEventListener("dragover",dragOver());
+  tile.addEventListener("dragstop",dragStop());
+  tile.addEventListener("dragdrop",dragDrop());
+}
+
+function dragStart(){
+    currTile=this;
+}
+function dragOver(e){
+    e.preventDefault();
+}
+function dragEnter(e){
+    e.preventDefault();
+}
+function dragLeave(){
+
+}
+function dragDrop(){
+    otherTile=this;
+}
+function dragEnd(){
+    let temp=currTile;
+    let currTile=otherTile;
+    let otherTile=temp;
+}
+
